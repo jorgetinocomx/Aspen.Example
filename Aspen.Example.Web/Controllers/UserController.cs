648720b0f87cd11346061b2b2aec99af2b498c90
Jorge Tinoco <jorgel.tinoco@gmail.com>
2023-03-06 11:51:25 -0600

    feat: print users in app

---
+++ Aspen.Example.Web/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Aspen.Example.DataAccess.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aspen.Example.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserData _data;
+
+
+        /// <summary>
+        /// Injects the dependencies.
+        /// </summary>
+        /// <param name="logger">MS logger.</param>
+        public UserController(ILogger<UserController> logger, IUserData data)
+        {
+            _logger = logger;
+            _data = data;
+        }
+
+        /// <summary>
+        /// Prints all the users.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var allUsers = _data
+                            .Get()
+                            .ToList();
+
+            return View(allUsers);
+        }
+    }
+}
